c410d15851a8cba3fec515448140d885ce8f6a6f
hakprj <hak.yan.project@gmail.com>
2026-02-22 20:00:00 +0100

    force save

--- gaussian_splatting/train.py
+++ gaussian_splatting/train.py
@@ -217,7 +217,9 @@ if __name__ == "__main__":
     # Start GUI server, configure and run training
     network_gui.init(args.ip, args.port)
     torch.autograd.set_detect_anomaly(args.detect_anomaly)
-    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
+    #force set
+    checkpoint_iterations = args.iterations
+    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, checkpoint_iterations, args.start_checkpoint, args.debug_from)
 
     # All done
     print("\nTraining complete.")
